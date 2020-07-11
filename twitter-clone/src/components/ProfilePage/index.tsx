92b0fd8ccd6cd41f950bac156c61c6b19d13856e
LorenaMendes <lorenamendes.contato@gmail.com>
2020-07-11 19:56:35 -0300

    Profile page component added

---
+++ twitter-clone/src/components/ProfilePage/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import {
+    Container,
+    Banner,
+    Avatar,
+    ProfileData,
+    EditButton,
+    LocationIcon,
+    CakeIcon,
+    Followage
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+        <Banner>
+            <Avatar />
+        </Banner>
+
+        <ProfileData>
+            <EditButton>Editar perfil</EditButton>
+            <h1>Lorena Mendes</h1>
+            <h2>@lolamendes</h2>
+
+            <p>Computer Science student at UFMG</p>
+
+            <ul>
+                <li>
+                    <LocationIcon />
+                    Belo Horizonte, Brasil
+                </li>
+                <li>
+                    <CakeIcon />
+                    Nascido(a) em 26 de junho de 1998
+                </li>
+            </ul>
+            <Followage>
+                <span>
+                    seguindo <strong>214</strong>
+                </span>
+                <span>
+                    <strong>558</strong> seguidores
+                </span>
+            </Followage>
+        </ProfileData>
+    </Container>
+  );
+}
+
+export default ProfilePage;
