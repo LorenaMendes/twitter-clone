32238fa1803ad4d5bf6ee027d6ad635cbd0e861b
LorenaMendes <lorenamendes.contato@gmail.com>
2020-07-11 18:38:34 -0300

    Main component added

---
+++ twitter-clone/src/components/Main/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo, BottomMenu, HomeIcon, SearchIcon, BellIcon, EmailIcon } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+        <Header>
+            <button>
+                <BackIcon />
+            </button>
+            <ProfileInfo>
+                <strong>Lorena Mendes</strong>
+                <span>58 Tweets</span>
+            </ProfileInfo>
+        </Header>
+
+        {/* <ProfilePage /> */}
+        
+        <BottomMenu>
+            <HomeIcon />
+            <SearchIcon />
+            <BellIcon />
+            <EmailIcon />
+        </BottomMenu>
+    </Container>
+  );
+}
+
+export default Main;
