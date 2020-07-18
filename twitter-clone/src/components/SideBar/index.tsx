95712b2ca5e0770f2496d674ec749e15d9e71306
LorenaMendes <lorenamendes.contato@gmail.com>
2020-07-18 12:03:16 -0300

    Side bar component added

---
+++ twitter-clone/src/components/SideBar/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import StickyBox from 'react-sticky-box';
+
+import List from '../List';
+import FollowSuggestion from '../FollowSuggestion';
+import News from '../News';
+
+import {
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
+} from './styles';
+
+const SideBar: React.FC = () => {
+  return (
+    <Container>
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+
+      <StickyBox>
+        <Body>
+          <List
+            title="Talvez você curta"
+            elements={[
+              <FollowSuggestion
+                name="Guilherme Renna"
+                nickname="@guilhermerenna"
+              />,
+              <FollowSuggestion name="Renatto Mendez" nickname="@xdrenatto" />,
+              <FollowSuggestion
+                name="Laura Abreu"
+                nickname="@lauraabreu"
+              />,
+            ]}
+          />
+          <List
+            title="O que está acontecendo"
+            elements={[
+              <News />,
+              <News />,
+              <News />,
+              <News />,
+              <News />,
+              <News />,
+              <News />,
+              <News />,
+              <News />,
+            ]}
+          />
+        </Body>
+      </StickyBox>
+    </Container>
+  );
+};
+
+export default SideBar;
